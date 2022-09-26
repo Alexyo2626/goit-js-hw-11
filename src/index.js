693591108bcbe8913ee6d864b714feb79b0796f8
Aleksey Knigin <alexyo2626@gmail.com>
2022-09-26 18:42:22 +0300

    add task

---
+++ src/index.js
@@ -0,0 +1,125 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import Notiflix from 'notiflix';
+import API from './js/fetchPhoto.js';
+
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+  inputEl: document.querySelector('input[name="searchQuery"]'),
+  formEl: document.querySelector('#search-form'),
+  moreBtn: document.querySelector('.load-more'),
+};
+
+const photoAPI = new API();
+let searchQuery = null;
+let totalPages = null;
+
+refs.formEl.addEventListener('submit', seachPhotoCard);
+refs.moreBtn.addEventListener('click', loadMore);
+
+function loadMore() {
+  photoAPI.incrementPage();
+  photoAPI.fetchPhoto(searchQuery).then(appendPhotoMurkup).catch(onFetchError);
+}
+
+function seachPhotoCard() {
+  event.preventDefault();
+  if (searchQuery != refs.inputEl.value) {
+    refs.galleryEl.innerHTML = '';
+  }
+  searchQuery = refs.inputEl.value.trim();
+  if (searchQuery === '') {
+    refs.moreBtn.classList.add('is-hidden');
+    return;
+  }
+  photoAPI.resetPage();
+  photoAPI
+    .fetchPhoto(searchQuery)
+    // .then(appendPhotoMurkup)
+    .then(data => {
+      if (data.hits.length === 0) {
+        refs.moreBtn.classList.add('is-hidden');
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+      appendPhotoMurkup(data);
+    })
+    .catch(onFetchError);
+}
+
+function appendPhotoMurkup(photoCard) {
+  refs.moreBtn.classList.remove('is-hidden');
+  totalPages = Math.ceil(photoCard.totalHits / 40);
+  if (totalPages === photoAPI.currentPage()) {
+    refs.moreBtn.classList.add('is-hidden');
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+  refs.galleryEl.insertAdjacentHTML('beforeend', createPhotoMarkup(photoCard));
+
+  var lightbox = new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  lightbox.refresh();
+}
+
+function onFetchError() {
+  error => {
+    if (data.hits.length === 0) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    console.log(error);
+  };
+}
+
+function createPhotoMarkup(photoCard) {
+  return photoCard.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <div class="photo-card">
+      <div class="thumb">
+   <a href="${largeImageURL}" class="gallery__item"><img src="${webformatURL}" class="gallery__image" width="290px" height="200px" alt="${tags}" loading="lazy"/></a>
+   </div>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+}
---
+++ src/js/fetchPhoto.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '30129874-c78727ddf7e732cad9221ad6f';
+const imageType = 'photo';
+const orientation = 'horizontal';
+const safesearch = 'true';
+const perPage = '10';
+
+export default class API {
+  constructor() {
+    this.page = 1;
+  }
+
+  async fetchPhoto(search) {
+    const url = `${BASE_URL}?key=${API_KEY}&q=${search}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${this.page}&per_page=${perPage}`;
+
+    const { data } = await axios.get(url);
+    return data;
+  }
+
+  currentPage() {
+    return this.page;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+}
